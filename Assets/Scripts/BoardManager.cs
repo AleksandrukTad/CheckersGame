0ec486951c23030b2576815fb5997cfa5baa5a26
Aleksandruk T (FCES) <15012840@students.southwales.ac.uk>
2017-12-18 19:08:27 +0000

    Setting up the board

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+	public Piece[,] pieces = new Piece[8, 8];
+	public GameObject whitePiecePrefab;
+	public GameObject blackPiecePrefab;
+
+	public Vector3 boardOffset = new Vector3(-4.0f, 0, -4.0f);
+	public Vector3 pieceOffset = new Vector3 (0.5f, 0, 0.5f);
+
+	private void Start()
+	{
+		generateBoard ();
+	}
+	private void generateBoard(){
+		//generate White team.
+		for (int y = 0; y < 3; y++) {
+			bool oddRow = (y % 2 == 0);
+			for (int x = 0; x < 8; x += 2) {
+				//generate piece
+				//ternery operator
+				GeneratePiece((oddRow)?x:x+1, y);
+			}
+		}
+
+		//generate Black team.
+		for (int y = 7; y > 4; y--) {
+			bool oddRow = (y % 2 == 0);
+			for (int x = 0; x < 8; x += 2) {
+				//generate piece
+				//ternery operator
+				GeneratePiece((oddRow)?x:x+1, y);
+			}
+		}
+	}
+
+	private void GeneratePiece(int x, int y){
+		bool isWhite = (y > 3) ? false : true;
+		GameObject go = Instantiate ((isWhite)?whitePiecePrefab : blackPiecePrefab) as GameObject;
+		go.transform.SetParent (transform);
+		Piece p = go.GetComponent<Piece> ();
+		pieces [x, y] = p;
+		movePiece (p, x, y);
+	}
+
+	private void movePiece(Piece p, int x, int y){
+		p.transform.position = (Vector3.right * x) + (Vector3.forward * y) + boardOffset +pieceOffset;
+	}
+}
