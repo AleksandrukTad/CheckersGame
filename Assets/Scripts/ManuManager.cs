c3f4354e9f59a124e0ba275fd411f68ad6f949c4
Aleksandruk T (FCES) <15012840@students.southwales.ac.uk>
2018-03-26 17:32:57 +0100

    UI for multiplayer done.

---
+++ Assets/Scripts/ManuManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManuManager : MonoBehaviour {
+	public static ManuManager Instance { set; get; }
+
+	public GameObject mainMenu;
+	public GameObject hostMenu;
+	public GameObject connectMenu;
+
+	public GameObject serverPrefab;
+	public GameObject clientPrefab;
+
+	public InputField nameInput;
+
+	private void Awake()
+	{
+		hostMenu.SetActive (false);
+		connectMenu.SetActive (false);
+		DontDestroyOnLoad (gameObject);
+	}
+
+	public void OnConnectBtn()
+	{
+		mainMenu.SetActive (false);
+		connectMenu.SetActive (true);
+	}
+	public void HostBtn()
+	{
+		try{
+			Server s = Instantiate(serverPrefab).GetComponent<Server>();
+			s.init();
+
+			Client c = Instantiate(clientPrefab).GetComponent<Client>();
+			c.clientName = nameInput.text;
+			c.ConnectToServer("127.0.0.1", 6321);
+
+		}catch(Exception e) {
+			Debug.Log (e.Message);
+		}
+		mainMenu.SetActive (false);
+		hostMenu.SetActive (true);
+	}
+	public void ConnectToServerBtn() 
+	{
+		string hostAddress = GameObject.Find ("HostInput").GetComponent<InputField> ().text;
+		if(hostAddress == "")
+			hostAddress = "127.0.0.1";
+
+		try
+		{
+			Client c = Instantiate(clientPrefab).GetComponent<Client>();
+			c.ConnectToServer(hostAddress, 6321);
+			connectMenu.SetActive(false);
+		}
+		catch(Exception e) {
+			Debug.Log (e.Message);
+		}
+	}
+	public void BackBtn()
+	{
+		hostMenu.SetActive (false);
+		connectMenu.SetActive (false);
+		mainMenu.SetActive (true);
+
+		Server s = FindObjectOfType<Server> ();
+		Client c = FindObjectOfType<Client> ();
+		if(s != null)
+			Destroy (s);
+		if (c != null)
+			Destroy (c);
+	}
+}
---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class Server : MonoBehaviour {
+	//which port game is going to be played on.
+	public int port = 6321;
+
+	private List<ServerClient> clients;
+	private List<ServerClient> disconnectList;
+
+	private TcpListener server;
+	private bool serverStarted;
+
+	private void Update(){
+		if (!serverStarted)
+			return;
+
+		foreach (var c in clients) {
+			//is the client still connected?
+			if (!IsConnected (c.tcp)) {
+				c.tcp.Close ();
+				disconnectList.Add (c);
+				continue;
+			} else {
+				//check if client writes something
+				NetworkStream s = c.tcp.GetStream();
+				if (s.DataAvailable) {
+					StreamReader reader = new StreamReader (s, true);
+					string data = reader.ReadLine ();
+
+					if (data != null)
+						OnIncomingData (c, data);
+				}
+			}
+		}
+		for (int i = 0; i < disconnectList.Count; i++) {
+			clients.Remove (disconnectList [i]);
+			disconnectList.RemoveAt (i);
+		}
+	}
+	public void init(){
+		DontDestroyOnLoad (gameObject);
+		clients = new List<ServerClient> ();
+		disconnectList = new List<ServerClient> ();
+
+		try{
+			server = new TcpListener(IPAddress.Any, port);
+			server.Start();
+			//listening for incomming connections
+			StartListening();
+		}
+		catch(Exception e) {
+			Debug.Log ("Socket error: " + e.Message);
+		}
+	}
+	private void StartListening(){
+		server.BeginAcceptTcpClient (AcceptTcpClient, server);
+	}
+	private void AcceptTcpClient(IAsyncResult ar){
+		TcpListener listener = (TcpListener)ar.AsyncState;
+		ServerClient sc = new ServerClient (listener.EndAcceptTcpClient (ar));
+		clients.Add (sc);
+		//tell server to comeback to listening, because after the ServerClient is added, server "forgets" to go back to listening
+		StartListening();
+		Debug.Log ("Somebody has connected!");
+	}
+	private bool IsConnected(TcpClient c){
+		try{
+			if(c != null && c.Client != null && c.Client.Connected){
+				return true;
+			}
+			else
+				return false;
+		}
+		catch{
+			return false;
+		}
+	}
+	//Send from server
+	private void Boardcast(string data, List<ServerClient> cl)
+	{
+		foreach (var sc in cl) {
+			try{
+				StreamWriter writer = new StreamWriter(sc.tcp.GetStream());
+				writer.WriteLine(data);
+				writer.Flush();
+			}
+			catch(Exception e){
+				Debug.Log ("Write error :" + e.Message);
+			}
+		}
+	}
+	//Server Read
+	private void OnIncomingData(ServerClient c, string data)
+	{
+		Debug.Log (c.clientName + " : " + data);
+	}
+}
+
+public class ServerClient
+{
+	public string clientName;
+	public TcpClient tcp;
+
+	public ServerClient (TcpClient tcp)
+	{
+		this.tcp = tcp;
+	}
+}
